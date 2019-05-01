cd52769032392e4ce2ef5c49a4f7abe873d0afcb
Alan <Alan@DESKTOP-40M27BF>
2019-05-01 06:33:44 -0300

    Problemas "resueltos"

---
+++ Prácticas de laboratorio/Prácticas de laboratorio/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace Prácticas_de_laboratorio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            double promedio = 0.0;
+            int coleccionDeNumeros = 0;
+            double[] numeros = new double[coleccionDeNumeros];
+            bool esNumero = false;
+            do
+            {
+                Console.WriteLine("Cuántos números necesita?");
+                esNumero = int.TryParse(Console.ReadLine(), out coleccionDeNumeros);
+                numeros = new double[coleccionDeNumeros];
+
+
+                if (esNumero)
+                {
+                    for (int i = 0; i < numeros.Length; i++)
+                    {
+                        bool esNumeroInterno = false;
+
+                        do {
+                            Console.WriteLine($"Ingrese número {i + 1}");
+                            esNumeroInterno = double.TryParse(Console.ReadLine(), out numeros[i]);
+
+                            if(esNumeroInterno)
+                            {
+                                promedio += numeros[i];
+                            }
+                            else
+                            {
+                                Console.WriteLine("Ingrese un número por favor");
+                            }
+
+                        } while (!esNumeroInterno);             
+
+                        if (numeros.Length == i + 1)
+                        {
+                            promedio /= numeros.Length;
+
+                        }
+                    }
+
+                }
+                else
+                {
+                    Console.WriteLine("Escriba un número");
+                }
+            } while (!esNumero);
+            Console.WriteLine($"El promedio es {promedio}");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Prácticas de laboratorio/tp2/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace tp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double mayor = 0.0;
+            int coleccionDeNumeros = 0;
+            double[] numeros = new double[coleccionDeNumeros];
+            bool esNumero = false;
+            string mediador = "";
+
+            do
+            {
+                Console.WriteLine("Cuántos números necesita?");
+                mediador = Console.ReadLine();
+                mediador = mediador.Replace(" ", "");
+                esNumero = int.TryParse(mediador, out coleccionDeNumeros);
+                if (esNumero && coleccionDeNumeros > 0)
+                {
+                    numeros = new double[coleccionDeNumeros];
+
+                
+                    for (int i = 0; i < numeros.Length; i++)
+                    {
+                        bool esNumeroInterno = false;
+                        string mediadorInterno = "";
+
+                        do
+                        {
+                            Console.WriteLine($"Ingrese número {i + 1}");
+                            mediadorInterno = Console.ReadLine();
+                            mediadorInterno = mediadorInterno.Replace(" ", "");
+                            esNumeroInterno = double.TryParse(mediadorInterno, out numeros[i]);
+
+                            if (esNumeroInterno)
+                            {
+                                if (numeros[i] > mayor)
+                                {
+                                    mayor = numeros[i];
+                                }
+                            }
+                            else
+                            {
+                                Console.WriteLine("Ingrese un número por favor");
+                            }
+
+                        } while (!esNumeroInterno);
+
+                        if (numeros.Length == i + 1)
+                        {
+                            Console.WriteLine($"El número más grande es {mayor}");
+
+                        }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Ingrese un número válido");
+                }
+            } while (!esNumero || coleccionDeNumeros <= 0);
+        } 
+    }
+}
+
