6654b7cd1368afff3b9e315ba2008b28dc770030
Alan <Alan@DESKTOP-40M27BF>
2019-05-01 22:33:04 -0300

    Pusheando código

--- Prácticas de laboratorio/tp5 for/Program.cs
+++ Prácticas de laboratorio/tp5 for/Program.cs
@@ -7,22 +7,30 @@ namespace tp5_for
         static void Main(string[] args)
         {
             double valor = 0.0;
-
-            Console.WriteLine("Ingrese el valor");
-            valor = double.Parse(Console.ReadLine());
-
-            for (int i = 0; i <= valor; i= i+2)
+            bool esNumero = false;
+            do
             {
-                if (i == valor)
-                {
-                    Console.WriteLine("es par");
+                Console.WriteLine("Ingrese el valor");
+                esNumero = double.TryParse(Console.ReadLine(), out valor);
+                if(esNumero){
+                    for (int i = 0; i <= valor; i = i + 2)
+                    {
+                        if (i == valor)
+                        {
+                            Console.WriteLine("es par");
+                        }
+
+                        if (i == valor - 1)
+                        {
+                            Console.WriteLine("es impar");
+                        }
+                    }
                 }
-                
-                if(i == valor - 1)
+                else
                 {
-                    Console.WriteLine("es impar");
+                    Console.WriteLine("Introduzca un número");
                 }
-            }
+            } while (!esNumero);
         }
     }
 }
